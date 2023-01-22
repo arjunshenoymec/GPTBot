2f7bf46f42b8d586aaa4af362df1efa016962739
Arjun Shenoy <arjunshenoymec@gmail.com>
2023-01-22 13:50:59 +0000

    removing run-bot.sh helper script and migrating to using config.py

---
+++ GPTChatBot/config.py
@@ -0,0 +1,13 @@
+import os
+
+# Slack bot token used by the bot to access various resources in the workspace
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+
+# Slack singing secret
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+
+# Slack app token
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+
+# Open ai token to access the openai endpoints
+OPENAI_TOKEN = os.environ["OPENAI_TOKEN"]
